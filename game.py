ae62747bb08426e94bc83af61bd5b6f53871a529
Dominik <domimik.kl@wp.pl>
2023-06-15 22:51:42 +0200

    quickfix

--- game.py
+++ game.py
@@ -1,7 +1,23 @@
 import pygame, sys
 from functions import renderBlocks, collisionCheck
 
-def Game(width, height, display, acceleration, x , y):
+
+
+pygame.init()
+
+width, height = 840, 1000 #1340
+display = pygame.display.set_mode((width, height))
+acceleration = 0
+
+
+y = 400
+x = 400
+
+
+while True:
+    # Game(width, height, display, acceleration, x, y)
+
+# def Game(width, height, display, acceleration, x , y):
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT or pygame.key.get_pressed()[pygame.K_ESCAPE]:
@@ -19,15 +35,16 @@ def Game(width, height, display, acceleration, x , y):
 
     y += acceleration
 
-    if acceleration < 10: acceleration += 0.1 #on se cały czas dodaje 0.1
+    if acceleration < 10: acceleration += 0.1
     if y >= height - 40: acceleration = 0
 
 
-    pygame.draw.circle(display, (0, 0, 0), [x - 80, y + 20], 20)
+    # pygame.draw.circle(display, (0, 0, 0), [x - 80, y + 20], 20)
     pygame.draw.rect(display, (0, 0, 0), pygame.Rect(x, y, 40, 40))
     renderBlocks(display)
 
     if collisionCheck(x, y): acceleration = 0
 
-    # pygame.time.Clock().tick(70)
+    pygame.time.Clock().tick(120)
     pygame.display.flip()
+
